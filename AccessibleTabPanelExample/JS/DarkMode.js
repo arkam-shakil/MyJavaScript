551aba5d6dcadb23c9fe981a0c2f421a0410aa57
Arkam <Arkamshakil@gmail.com>
2021-09-25 23:28:05 +0530

    Added a keyboard command for darkmode

--- AccessibleTabPanelExample/JS/DarkMode.js
+++ AccessibleTabPanelExample/JS/DarkMode.js
@@ -1,18 +1,39 @@
-//Code for Dark Mode
+function keyCommandForDarkMode(event)
+{
+	let keyPressedByUser = event.key;
+
+	if (event.ctrlKey && event.which == 50) {
+		darkMode();
+	}
+}
+
+
 $(document).ready(function()
 {
-	$("#darkmode").click(function()
-	{
-		let ariaPressedValue = $(this).attr("aria-pressed");
-		
-		if (ariaPressedValue === "false")
-		{
-			$(this).attr("aria-pressed", "true");
-			$("body, main, h1").attr("style", "background-color: black; color: white;");
-		}
-		else {
-			$(this).attr("aria-pressed", "false");
-			$("body, main, h1").removeAttr("style");
-		}
-	});
+	$("#darkmode").click(darkMode);
 });
+
+function darkMode()
+{
+	let ariaPressedValue = $("#darkmode").attr("aria-pressed");
+
+	if (ariaPressedValue === "false")
+	{
+		$("#darkmode").attr("aria-pressed", "true");
+		$("body, main, h1").attr("style", "background-color: black; color: white;");
+			document.getElementById("liveMessage").removeAttribute("style");
+			document.getElementById("liveMessage").innerHTML = "Dark Mode enabled.";
+			setTimeout(function() {
+			document.getElementById('liveMessage').innerHTML = '';
+			document.getElementById("liveMessage").setAttribute("style", "display: none;");}, 3000);
+	}
+	else {
+		$("#darkmode").attr("aria-pressed", "false");
+		$("body, main, h1").removeAttr("style");
+			document.getElementById("liveMessage").removeAttribute("style");
+			document.getElementById("liveMessage").innerHTML = "Light Mode enabled.";
+			setTimeout(function() {
+			document.getElementById('liveMessage').innerHTML = '';
+			document.getElementById("liveMessage").setAttribute("style", "display: none;");}, 3000);
+	}
+}
